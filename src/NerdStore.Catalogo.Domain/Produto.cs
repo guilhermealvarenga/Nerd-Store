ea12a1c7b89879504f38d084e5c1ee3951cda50a
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-04-25 20:20:49 -0300

    Agregando objetos de valor

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -6,31 +6,19 @@ namespace NerdStore.Catalogo.Domain
     public class Produto : Entity, IAggregateRoot
     {
         public Guid CategoriaId { get; private set; }
-
         public string Nome { get; private set; }
-
         public string Descricao { get; private set; }
-
         public bool Ativo { get; private set; }
-
         public decimal Valor { get; private set; }
-
         public DateTime DataCadastro { get; private set; }
-
         public string Imagem { get; private set; }
-
         public int QuantidadeEstoque { get; private set; }
-
+        public Dimensoes Dimensoes { get; private set; }
         public Categoria Categoria { get; private set; }
 
-        public Produto(
-            string nome,
-            string descricao,
-            bool ativo,
-            decimal valor,
-            Guid categoriaId,
-            DateTime dataCadastro,
-            string imagem)
+        protected Produto() { }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, Dimensoes dimensoes)
         {
             CategoriaId = categoriaId;
             Nome = nome;
@@ -39,6 +27,7 @@ namespace NerdStore.Catalogo.Domain
             Valor = valor;
             DataCadastro = dataCadastro;
             Imagem = imagem;
+            Dimensoes = dimensoes;
 
             Validar();
         }
