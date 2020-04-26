d3f857b68196d5645dbe6311201ed8bb71aa71c4
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-04-25 22:36:03 -0300

    Persistindo com repositórios

--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -1,12 +1,16 @@
-﻿using NerdStore.Core.DomainObjects;
+﻿using System.Collections.Generic;
+using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Catalogo.Domain
 {
     public class Categoria : Entity
     {
         public string Nome { get; private set; }
-
         public int Codigo { get; private set; }
+        // EF Relation
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria() { }
 
         public Categoria(string nome, int codigo)
         {
