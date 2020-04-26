a1f4f8c78c93c665a271fe958590822023911428
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-04-26 13:26:02 -0300

    Fix para rodar migrations do Entity Framework no Mac OS

---
+++ src/NerdStore.Catalogo.Data/DesignTimeCatalogoContextFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NerdStore.Catalogo.Data
+{
+    public class DesignTimeCatalogoContextFactory
+    {
+        public DesignTimeCatalogoContextFactory()
+        {
+        }
+    }
+}
