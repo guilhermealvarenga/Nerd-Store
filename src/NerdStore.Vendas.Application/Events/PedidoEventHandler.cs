0f633cd56b0181f14c687a1426cd4929981ef137
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-05-02 11:32:40 -0300

    Integração de BC's - Processamento do pedido

--- src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
+++ src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
@@ -2,13 +2,15 @@
 using System.Threading.Tasks;
 using MediatR;
 using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
 
 namespace NerdStore.Vendas.Application.Events
 {
     public class PedidoEventHandler :
         INotificationHandler<PedidoRascunhoIniciadoEvent>,
         INotificationHandler<PedidoAtualizadoEvent>,
-        INotificationHandler<PedidoItemAdicionadoEvent>
+        INotificationHandler<PedidoItemAdicionadoEvent>,
+        INotificationHandler<PedidoEstoqueRejeitadoEvent>
     {
 
         private readonly IMediatorHandler _mediatorHandler;
@@ -31,6 +33,12 @@ namespace NerdStore.Vendas.Application.Events
         public Task Handle(PedidoItemAdicionadoEvent notification, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
-        }       
+        }
+
+        public Task Handle(PedidoEstoqueRejeitadoEvent notification, CancellationToken cancellationToken)
+        {
+            // cancelar o processamento do pedido - retornar erro para o cliente.
+            return Task.CompletedTask;
+        }
     }
 }
--- src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
+++ src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -111,29 +111,29 @@ namespace NerdStore.WebApp.MVC.Controllers
             return View("Index", await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
         }
 
-        //[Route("resumo-da-compra")]
-        //public async Task<IActionResult> ResumoDaCompra()
-        //{
-        //    return View(await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
-        //}
+        [Route("resumo-da-compra")]
+        public async Task<IActionResult> ResumoDaCompra()
+        {
+            return View(await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
+        }
 
-        //[HttpPost]
-        //[Route("iniciar-pedido")]
-        //public async Task<IActionResult> IniciarPedido(CarrinhoViewModel carrinhoViewModel)
-        //{
-        //    var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
+        [HttpPost]
+        [Route("iniciar-pedido")]
+        public async Task<IActionResult> IniciarPedido(CarrinhoViewModel carrinhoViewModel)
+        {
+            var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
 
-        //    var command = new IniciarPedidoCommand(carrinho.PedidoId, ClienteId, carrinho.ValorTotal, carrinhoViewModel.Pagamento.NomeCartao,
-        //        carrinhoViewModel.Pagamento.NumeroCartao, carrinhoViewModel.Pagamento.ExpiracaoCartao, carrinhoViewModel.Pagamento.CvvCartao);
+            var command = new IniciarPedidoCommand(carrinho.PedidoId, ClienteId, carrinho.ValorTotal, carrinhoViewModel.Pagamento.NomeCartao,
+                carrinhoViewModel.Pagamento.NumeroCartao, carrinhoViewModel.Pagamento.ExpiracaoCartao, carrinhoViewModel.Pagamento.CvvCartao);
 
-        //    await _mediatorHandler.EnviarComando(command);
+            await _mediatorHandler.EnviarComando(command);
 
-        //    if (OperacaoValida())
-        //    {
-        //        return RedirectToAction("Index", "Pedido");
-        //    }
+            if (OperacaoValida())
+            {
+                return RedirectToAction("Index", "Pedido");
+            }
 
-        //    return View("ResumoDaCompra", await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
-        //}
+            return View("ResumoDaCompra", await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
+        }
     }
 }
