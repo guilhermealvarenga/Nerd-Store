a2284e61dd207728d9298a9f3eaee267a9676756
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-05-03 23:05:57 -0300

    Setup do ES no projeto

--- src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
+++ src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
@@ -9,7 +9,6 @@ namespace NerdStore.Vendas.Application.Events
 {
     public class PedidoEventHandler :
         INotificationHandler<PedidoRascunhoIniciadoEvent>,
-        INotificationHandler<PedidoAtualizadoEvent>,
         INotificationHandler<PedidoItemAdicionadoEvent>,
         INotificationHandler<PedidoEstoqueRejeitadoEvent>,
         INotificationHandler<PagamentoRealizadoEvent>,
@@ -26,12 +25,7 @@ namespace NerdStore.Vendas.Application.Events
         public Task Handle(PedidoRascunhoIniciadoEvent message, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
-        }
-
-        public Task Handle(PedidoAtualizadoEvent message, CancellationToken cancellationToken)
-        {
-            return Task.CompletedTask;
-        }
+        }       
 
         public Task Handle(PedidoItemAdicionadoEvent message, CancellationToken cancellationToken)
         {
--- src/NerdStore.Vendas.Application/Events/PedidoProdutoAdicionadoEvent.cs
+++ src/NerdStore.Vendas.Application/Events/PedidoProdutoAdicionadoEvent.cs
@@ -3,18 +3,20 @@ using NerdStore.Core.Messages;
 
 namespace NerdStore.Vendas.Application.Events
 {
-    public class PedidoAtualizadoEvent : Event
+    public class PedidoProdutoAdicionadoEvent : Event
     {
         public Guid ClienteId { get; private set; }
         public Guid PedidoId { get; private set; }
-        public decimal ValorTotal { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public int Quantidade { get; private set; }
 
-        public PedidoAtualizadoEvent(Guid clienteId, Guid pedidoId, decimal valorTotal)
+        public PedidoProdutoAdicionadoEvent(Guid clienteId, Guid pedidoId, Guid produtoId, int quantidade)
         {
             AggregateId = pedidoId;
             ClienteId = clienteId;
             PedidoId = pedidoId;
-            ValorTotal = valorTotal;
+            ProdutoId = produtoId;
+            Quantidade = quantidade;
         }
     }
 }
